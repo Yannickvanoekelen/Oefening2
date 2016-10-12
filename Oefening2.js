95c998a3f81eb5647df6d6406ca240dc32896271
Yannick Van Oekelen <r0298651@student.thomasmore.be>
2016-10-12 17:59:43 +0200

    Dit is mijn eerste testfile via WebStorm

--- Oefening2.js
+++ Oefening2.js
@@ -3,5 +3,22 @@
  */
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+
+app.listen(4567);
+
+var  books = [];
+books.push({id:1, name:"Beginning Node Js", rating:4});
+books.push({id:2, name:"Knoeien met data-distributie", rating:3});
+
+
+app.get("/books", function(request, response) {
+    response.send({name:"Beginning Node JS", rating:4})
+});
+
+
+app.post("/books", function(request, response) {
+    response.status(204).send();
+});
 
 console.log("woow, dees werkt!");
